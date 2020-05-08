86fbdb6e57ddc798ad3239c94a3a22e072525f4f
Stéphane Lepin <stephane.lepin@gmail.com>
2020-05-08 19:55:36 +0200

    ci: migrate to azure pipelines

---
+++ ci/windows/package-windows.cmd
@@ -0,0 +1,14 @@
+mkdir package
+cd package
+
+call ..\ci_config.inc.cmd
+
+git rev-parse --short HEAD > package-version.txt
+set /p PackageVersion=<package-version.txt
+del package-version.txt
+
+REM Package ZIP archive
+7z a "%PluginName%-%PackageVersion%-Windows.zip" "..\release\*"
+
+REM Build installer
+iscc ..\installer\installer-Windows.iss /O. /F"%PluginName%-%PackageVersion%-Windows-Installer"
