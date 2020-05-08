aeee4ea360172667aa71147e38e3ef195a8c4813
Stéphane Lepin <stephane.lepin@gmail.com>
2020-05-08 22:47:28 +0200

    ci(windows): fix generated files import

--- ci/windows/prepare-windows.cmd
+++ ci/windows/prepare-windows.cmd
@@ -1,7 +1,7 @@
 mkdir build32
 mkdir build64
 
-call ..\ci_includes.generated.cmd
+call "%~dp0..\ci_includes.generated.cmd"
 
 cd build32
 cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_SYSTEM_VERSION=10.0 -DQTDIR="%QTDIR32%" -DLibObs_DIR="%OBSPath%\build32\libobs" -DLIBOBS_INCLUDE_DIR="%OBSPath%\libobs" -DLIBOBS_LIB="%OBSPath%\build32\libobs\%build_config%\obs.lib" -DOBS_FRONTEND_LIB="%OBSPath%\build32\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib" ..
