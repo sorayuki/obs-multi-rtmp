6f05520dc9f3f3c449882d0a06722795a9237697
Jat <jat@sinosky.org>
2020-05-26 10:18:40 +0800

    linux build

---
+++ build_linux.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+set -eux
+
+: "${obs_include_dir:=/usr/include/obs}"
+
+ver=$(grep 'obs-multi-rtmp VERSION' CMakeLists.txt)
+ver=($ver)
+ver=${ver[2]}
+ver=${ver:0:-1}
+
+obs_ver=$(obs -V)
+obs_ver=${obs_ver//-/ }
+obs_ver=($obs_ver)
+obs_ver=${obs_ver[2]}
+
+rm -rf build dist
+curl -o /tmp/obs-frontend-api.h https://raw.githubusercontent.com/obsproject/obs-studio/$obs_ver/UI/obs-frontend-api/obs-frontend-api.h
+sudo mkdir -p $obs_include_dir/UI/obs-frontend-api
+sudo mv /tmp/obs-frontend-api.h $obs_include_dir/UI/obs-frontend-api
+
+cmake  -DCMAKE_INSTALL_PREFIX=/usr -DOBS_INCLUDE_DIR=$obs_include_dir -B build .
+cmake --build build --config Release
+sudo cmake --install build --config Release
