734e47655879699c1a73ec430f42592bbea85fc1
Jat <jat@sinosky.org>
2020-05-27 06:29:05 +0800

    fix windows build

--- build.bat
+++ build.bat
@@ -4,11 +4,11 @@ del release.zip
 set QTDIR32=-DQTDIR="F:/dev/qt/5.10.1/msvc2017"
 set QTDIR64=-DQTDIR="F:/dev/qt/5.10.1/msvc2017_64"
 
-cmake %QTDIR32% -G "Visual Studio 16 2019" -A Win32 -B build_x86 -S . -DCMAKE_INSTALL_PREFIX=dist
+cmake %QTDIR32% -DOBS_INCLUDE_DIR="obs-src" -G "Visual Studio 16 2019" -A Win32 -B build_x86 -S . -DCMAKE_INSTALL_PREFIX=dist
 cmake --build build_x86 --config Release
 cmake --install build_x86 --config Release
 
-cmake %QTDIR64% -G "Visual Studio 16 2019" -A x64 -B build_x64 -S . -DCMAKE_INSTALL_PREFIX=dist
+cmake %QTDIR64% -DOBS_INCLUDE_DIR="obs-src" -G "Visual Studio 16 2019" -A x64 -B build_x64 -S . -DCMAKE_INSTALL_PREFIX=dist
 cmake --build build_x64 --config Release
 cmake --install build_x64 --config Release
 
--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,9 @@
 
 set -eux
 
-: "${obs_include_dir:=/usr/include/obs}"
+: "${DESTDIR:=/usr}"
+: "${QTDIR:=}"
+: "${OBS_INCLUDE_DIR:=/usr/include/obs}"
 
 ver=$(grep 'obs-multi-rtmp VERSION' CMakeLists.txt)
 ver=($ver)
@@ -16,9 +18,9 @@ obs_ver=${obs_ver[2]}
 
 rm -rf build dist
 curl -o /tmp/obs-frontend-api.h https://raw.githubusercontent.com/obsproject/obs-studio/$obs_ver/UI/obs-frontend-api/obs-frontend-api.h
-sudo mkdir -p $obs_include_dir/UI/obs-frontend-api
-sudo mv /tmp/obs-frontend-api.h $obs_include_dir/UI/obs-frontend-api
+sudo mkdir -p $OBS_INCLUDE_DIR/UI/obs-frontend-api
+sudo mv /tmp/obs-frontend-api.h $OBS_INCLUDE_DIR/UI/obs-frontend-api
 
-cmake  -DCMAKE_INSTALL_PREFIX=/usr -DOBS_INCLUDE_DIR=$obs_include_dir -B build .
+cmake  -DCMAKE_INSTALL_PREFIX=$DESTDIR -DQTDIR=$QTDIR -DOBS_INCLUDE_DIR=$OBS_INCLUDE_DIR -B build .
 cmake --build build --config Release
-sudo cmake --install build --config Release
+# sudo cmake --install build --config Release
