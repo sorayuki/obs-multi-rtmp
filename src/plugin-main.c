623c7600e80224c0e7f52d12b2d2bed148736844
PatTheMav <PatTheMav@users.noreply.github.com>
2022-03-22 23:28:44 +0100

    Apply CMake format to main source file

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -25,11 +25,12 @@ OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
 bool obs_module_load(void)
 {
-    blog(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
-    return true;
+	blog(LOG_INFO, "plugin loaded successfully (version %s)",
+	     PLUGIN_VERSION);
+	return true;
 }
 
 void obs_module_unload()
 {
-    blog(LOG_INFO, "plugin unloaded");
+	blog(LOG_INFO, "plugin unloaded");
 }
