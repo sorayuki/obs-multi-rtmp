43792845cbfadacf538f6b227c1aae97a61d8038
Nucy666 <2285352317@qq.com>
2023-02-21 17:35:58 +0800

    显示推流时长和推流bps

--- src/push-widget.h
+++ src/push-widget.h
@@ -5,7 +5,8 @@ public:
     virtual ~PushWidget() {}
     virtual QJsonObject Config() = 0;
     virtual bool ShowEditDlg() = 0;
-
+    virtual bool StartStreaming() = 0;
+    virtual void StopStreaming() = 0;
     virtual void OnOBSEvent(obs_frontend_event ev) = 0;
 };
 
