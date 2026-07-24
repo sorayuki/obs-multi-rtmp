040cdfe6664c28223064b048448ed9fe5a8c767a
Nick Bolton <njbolton91@gmail.com>
2026-07-24 18:35:12 -0400

    fix: delete Notifier copy ctor/assignment to enforce singleton

--- src/notifier.h
+++ src/notifier.h
@@ -8,6 +8,8 @@ public:
     void NotifyFailure(const QString& targetName, const QString& reason);
 private:
     Notifier() = default;
+    Notifier(const Notifier&) = delete;
+    Notifier& operator=(const Notifier&) = delete;
     QSystemTrayIcon* tray_ = nullptr;
     void ensureTray();
 };
